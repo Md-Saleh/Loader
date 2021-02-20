0792cd94911db4065869984b19666158c7372a2a
User <mdabusalehbd5@gmail.com>
2021-02-21 02:08:00 +0600

    reaplace code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
-	var myVar;
-
-	function myFunction() {
-	  myVar = setTimeout(showPage, 3000);
-	}
-
-	function showPage() {
-	  document.getElementById("loading-area").style.display = "none";
-	  document.getElementById("layout-body").style.display = "block";
-	}
+	$(window).on('load', function() { // makes sure the whole site is loaded 
+	  $('#loading-area span').fadeOut(); // will first fade out the loading animation 
+	  $('#loading-area').delay(500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	  $('body').delay(500).css({'overflow':'visible'});
+	})
 });
